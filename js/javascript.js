56e77cde1cce4b7bb9766d1fa8b75e762263f8d5
Luis Amarilla <flopydepapi@gmail.com>
2022-06-07 19:01:45 -0300

    primer commit

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+
+
+function cambiarfondo1() { document.body.style.backgroundColor = "red";}
+
+function cambiarfondo2() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function cartel() {alert("Potrero Digital")}
+
+function roberto() {
+  document.getElementById("luis").innerHTML = "Roberto Carlos";
+}
+
+
+function AmpliarImg(x) {
+  x.style.height = "20%";
+  x.style.width = "20%";
+}
+
+function NormalImg(x) {
+  x.style.height = "5%";
+  x.style.width = "5%";
+}
